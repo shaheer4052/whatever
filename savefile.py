5eb39a3ca040b764babe5383b08dd166c6ec5a38
Shaheer <technogamerz2525@gmail.com>
2026-02-10 13:28:08 +0500

    SAVE FILE

--- savefile.py
+++ savefile.py
@@ -25,3 +25,40 @@ for char in user_input:
 
 print(f"\nFinal Result: {secret_msg}")
 print("Thanks for using the Caesar Cipher App!")
+
+#________________________   BANK ACCOUNT _________________________________
+class BankAccount:
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount <= 0:
+            print("Deposit amount must be positive.")
+            return
+        self.balance += amount
+        print(f"Deposited {amount}. New balance: {self.balance}")
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            print("Withdrawal amount must be positive.")
+            return
+        if amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance: {self.balance}")
+
+    def show_balance(self):
+        print(f"{self.name}'s balance is {self.balance}")
+
+
+acc1 = BankAccount("Ali", 5000)
+acc2 = BankAccount("Sara")
+
+acc1.deposit(1000)
+acc2.deposit(300)
+acc1.withdraw(2000)
+
+acc1.show_balance()
+acc2.show_balance()
